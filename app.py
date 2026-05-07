abb30703406314a5c686fd517e4a7d707c5f5fde
Gautham0603 <gauthamvishwanath.s2025@vitstudent.ac.in>
2026-05-07 15:07:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ url = "https://graphql.anilist.co"
 # Query
 query = """
 {
-  Page(page: 1, perPage: 50) {
+  Page(page: 30, perPage: 50) {
     media(type: ANIME, sort: POPULARITY_DESC) {
       title {
         romaji
